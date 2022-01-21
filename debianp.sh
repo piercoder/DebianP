cb71984d1b150d712e7e56a36f4cde2ebb6e5f40
piercoder <ppattitoni@gmail.com>
2022-01-21 20:04:54 +0100

    Update: Fri Jan 21 08:04:54 PM CET 2022

--- debianp.sh
+++ debianp.sh
@@ -12,24 +12,18 @@ echo '| #mmm"  "#mm"  ##m#"  mm#mm  "mm"#  #   #  #      |'
 echo '+==================================================+'
 
 echo ""
-echo "--------------------------------"
-echo "1 - Changing the repository list"
-echo "--------------------------------"
+echo "------------------------"
+echo "1 - Upgrading the system"
+echo "------------------------"
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
 sudo cp sources.list /etc/apt/sources.list
-echo "Repository list updated"
-
-echo ""
-echo "-------------------------"
-echo "2 - Upgrading your system"
-echo "-------------------------"
 sudo apt update && sudo apt full-upgrade && sudo apt autoremove
 sudo apt purge $(dpkg -l | grep "^rc" | awk '{print $2}')
 echo "System updated"
 
 echo ""
 echo "--------------------------------------"
-echo "3 - Installing the desktop environment"
+echo "2 - Installing the desktop environment"
 echo "--------------------------------------"
 sudo apt install xfce4 xfce4-goodies \
 				gnome-themes-extra papirus-icon-theme \
@@ -38,7 +32,7 @@ echo "Desktop environment installed"
 
 echo ""
 echo "----------------------------------"
-echo "4 - Installing additional packages"
+echo "3 - Installing additional packages"
 echo "----------------------------------"
 sudo apt install firefox-esr geany geany-common synaptic flatpak \
 				command-not-found mc htop neofetch cmatrix
@@ -48,10 +42,11 @@ echo "Additional packages installed"
 
 echo ""
 echo "-------------------------"
-echo "5- Configuring the system"
+echo "4- Configuring the system"
 echo "-------------------------"
 mkdir -p ~/bin ~/.wallpapers
 cp wallpaper.jpg ~/.wallpapers
+echo "TO BE DONE"
 echo "System configured"
 #TBD
 # GRUB
