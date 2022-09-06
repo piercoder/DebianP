addf6438f972aa919e0f48cfe220967e585ab2ec
pier <ppattitoni@gmail.com>
2022-09-06 18:12:00 +0200

    Update: Tue Sep  6 06:12:00 PM CEST 2022

--- debianp.sh
+++ debianp.sh
@@ -40,7 +40,7 @@ sudo apt purge xfce4-notifyd ## if you started from the xfce installation, unins
 sudo apt autoremove
 sudo apt purge $(dpkg -l | grep "^rc" | awk '{print $2}')
 sudo apt autoremove
-sudo apt install i3 feh picom kitty fonts-font-awesome lxappearance gtk2-engines-murrine dunst libnotify-bin flameshot xss-lock diodon network-manager-gnome slick-greeter lightdm-settings
+sudo apt install i3 feh picom kitty fonts-font-awesome lxappearance gtk2-engines-murrine dunst libnotify-bin flameshot xss-lock diodon network-manager-gnome slick-greeter lightdm-settings pasystray
 echo "< Desktop environment installed >"
 
 echo ""
