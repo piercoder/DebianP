2e5540c73b1ebc546309c7cba9cfaed958aee984
pier <ppattitoni@gmail.com>
2022-10-16 20:24:52 +0200

    Update: Sun Oct 16 08:24:52 PM CEST 2022

--- debianp.sh
+++ debianp.sh
@@ -68,7 +68,7 @@ echo "--------------------------------------"
 echo "2 - Installing the desktop environment"
 echo "--------------------------------------"
 echo ""
-sudo apt install -y man wget curl neofetch
+sudo apt install -y man wget curl neofetch deborphan
 echo "< System utitlities installed >"
 
 # Microcode for AMD/Intel
