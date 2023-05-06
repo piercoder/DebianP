38a5c30667789a8bbe4102154305de790c435178
piercoder <ppattitoni@gmail.com>
2023-05-06 10:12:01 +0200

    Update: Sat May  6 10:12:01 AM CEST 2023

--- themechanger.sh
+++ themechanger.sh
@@ -13,6 +13,62 @@ wd=$(pwd)
 
 
 
+# kitty
+# Define the file path of the text file
+file_path="$(pwd)/.config/kitty/kitty.conf"
+
+# Foreground
+start_text="foreground "
+new_text="foreground $neutral"
+sed -i "/^${start_text}/c${new_text}" ${file_path}
+
+# Background
+start_text="background "
+new_text="background $dark"
+sed -i "/^${start_text}/c${new_text}" ${file_path}
+
+# Selection foreground
+start_text="selection_foreground "
+new_text="selection_foreground $dark"
+sed -i "/^${start_text}/c${new_text}" ${file_path}
+
+# Selection background
+start_text="selection_background "
+new_text="selection_background $accent"
+sed -i "/^${start_text}/c${new_text}" ${file_path}
+
+# Cursor
+start_text="cursor "
+new_text="cursor $accent"
+sed -i "/^${start_text}/c${new_text}" ${file_path}
+
+
+
+
+
+# dunst
+# Define the file path of the text file
+file_path="$(pwd)/.config/dunst/dunstrc"
+
+# Frame color
+start_text="    frame_color = "
+new_text="    frame_color = \"$neutral\""
+sed -i "/^${start_text}/c${new_text}" ${file_path}
+
+# Substitute background color
+start_text="    background "
+new_text="dark=\"$dark\""
+sed -i "/^${start_text}/c${new_text}" ${file_path}
+
+# Substitute foreground color
+start_text="    foreground "
+new_text="light=\"$light\""
+sed -i "/^${start_text}/c${new_text}" ${file_path}
+
+
+
+
+
 # dmenu style
 # Define the file path of the text file
 file_path="$(pwd)/.config/i3/dmenubin/style"
